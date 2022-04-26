9d6a1323132005dc9f5a602670942d2c74f72f67
dope <mdc@kakao.com>
2022-04-26 09:55:48 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          df
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          dfg
-        </a>
+      <header className="header">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
       </header>
     </div>
   );
