1d27ecb8ca130abe2d9c8e6ded74d20fab1b66c2
dope <mdc@kakao.com>
2022-04-27 23:46:24 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,36 @@
 import "./home.css";
-<script
-  type="text/javascript"
-  src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=chnhlyto0x"
-></script>;
-var mapOptions = {
-  center: new naver.maps.LatLng(37.521602, 126.890874),
-  zoom: 10,
+import { RenderAfterNavermapsLoaded, NaverMap } from "react-naver-maps";
+const naverMapComponent = () => {
+  const id = "chnhlyto0x";
+  return (
+    <NaverMap
+      mapDivId={"react-naver-map"}
+      style={{
+        width: "100%",
+        height: "100vh",
+      }}
+      defaultCenter={{
+        lat: 37.521602,
+        lng: 126.890874,
+      }}
+      defaultZoom={10}
+    />
+  );
 };
-
-var map = new naver.maps.Map("map", mapOptions);
-
 const Home = () => {
   return (
     <div className="home">
       <div className="body">
         <div className="box">안녕하세용</div>
-        <div id="map" style="width:100%;height:400px;"></div>
+        <div>
+          <RenderAfterNavermapsLoaded
+            ncpClientId={"chnhlyto0x"}
+            error={<p>map load error</p>}
+            loading={<p>map loading ...</p>}
+          >
+            <naverMapComponent />
+          </RenderAfterNavermapsLoaded>
+        </div>
       </div>
     </div>
   );
