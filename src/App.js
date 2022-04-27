048560f687c46934438f8327dbe5c179b57f46ec
dope <mdc@kakao.com>
2022-04-27 14:29:34 +0900

    a

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
 
           <button
             onClick={addComment(name, comment, pw).then(() => {
-              window.location.replace("/");
+              window.location.replace("/hopedope");
             })}
           >
             추가
