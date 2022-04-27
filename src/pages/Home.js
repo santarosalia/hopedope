3b3679e89f33374ad6ed6e39d60aac6d20f5608e
dope <mdc@kakao.com>
2022-04-27 23:59:07 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,22 @@
 import "./home.css";
 import { RenderAfterNavermapsLoaded, NaverMap } from "react-naver-maps";
-const naverMapComponent = () => {
-  const id = "chnhlyto0x";
-  return (
-    <NaverMap
-      mapDivId={"react-naver-map"}
-      style={{
-        width: "100%",
-        height: "100vh",
-      }}
-      defaultCenter={{
-        lat: 37.521602,
-        lng: 126.890874,
-      }}
-      defaultZoom={10}
-    />
-  );
-};
+
 const Home = () => {
   return (
     <div className="home">
       <div className="body">
         <div className="box">안녕하세용</div>
-        <div>
-          <RenderAfterNavermapsLoaded
-            ncpClientId={"chnhlyto0x"}
-            error={<p>map load error</p>}
-            loading={<p>map loading ...</p>}
-          >
-            <naverMapComponent />
+        <div id="react-naver-map">
+          <RenderAfterNavermapsLoaded clientId={"chnhlyto0x"}>
+            <NaverMap
+              mapDivId={"react-naver-map"} // default: react-naver-map
+              style={{
+                width: "100%",
+                height: "400px",
+              }}
+              defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
+              defaultZoom={10}
+            />
           </RenderAfterNavermapsLoaded>
         </div>
       </div>
