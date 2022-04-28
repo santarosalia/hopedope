6713c6471c0637965115fc3ec6dfca8940fa8e87
dope <mdc@kakao.com>
2022-04-28 09:58:36 +0900

    a

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -1,10 +1,43 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
+
 import "./photo.css";
-const Photo = () => {
+
+const Photo = (props) => {
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+    },
+  ];
+
   return (
     <div className="photo">
       <h3>사진입니당</h3>
+      <div>
+        <Carousel>
+          {items.map((item, i) => (
+            <Item key={i} item={item} />
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
+  function Item(props) {
+    return (
+      <Paper>
+        <h2>{props.item.name}</h2>
+        <p>{props.item.description}</p>
+
+        <Button className="CheckButton">Check it out!</Button>
+      </Paper>
+    );
+  }
 };
 
 export default Photo;
