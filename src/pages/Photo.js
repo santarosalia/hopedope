d0504ef6fef4b1d743e8e55cd3cde805012d70e0
dope <mdc@kakao.com>
2022-04-28 15:03:25 +0900

    a

--- src/pages/Photo.js
+++ src/pages/Photo.js
@@ -1,11 +1,24 @@
 import "./photo.css";
+import { Swiper, SwiperSlide } from "swiper/react";
 import carousel from "./carousel";
+import "swiper/css";
 const Photo = () => {
   return (
     <div className="photo">
       <h3>사진입니당</h3>
       <div>
-        <carousel />
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          ...
+        </Swiper>
       </div>
     </div>
   );
