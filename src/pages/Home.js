be4d12a1ef0d979266a4dbcd139bb03cca978bc2
dope <mdc@kakao.com>
2022-04-29 00:41:03 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
             </NaverMap>
           </RenderAfterNavermapsLoaded>
         </div>
-        <a id="create-kakao-link-btn">
+        <a id="kakao-link-btn">
           <img
             src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
             alt="카카오링크 보내기 버튼"
