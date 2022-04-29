75d7d7bd2d56330168817ac9871550544b41bcf1
dope <mdc@kakao.com>
2022-04-29 10:43:21 +0900

    a

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
         <Route path="/hopedope/about" element={<About />} />
         <Route path="/hopedope/msg" element={<Msg />} />
         <Route path="/hopedope/photo" element={<Photo />} />
-        <Route path="*" element={<Home />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
       <footer>footer</footer>
     </div>
