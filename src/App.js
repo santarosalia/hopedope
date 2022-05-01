fac5b03411da2eba9a6a79c949cb505f7fb4bd8c
dope <mdc@kakao.com>
2022-05-01 22:27:06 +0900

    a

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import Msg from "./pages/Msg";
 import Photo from "./pages/Photo";
 import "./App.css";
 import Helmet from "react-helmet";
+import { useEffect } from "react";
 const App = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.text =
+      'Kakao.init("7d350e6dfaeae1fb62dd5827979d4a88");Kakao.Link.createScrapButton({container: "#kakao-link-btn",requestUrl: "https://santarosalia.github.io/hopedope/",templateId: 75771,templateArgs: {THU: "https://raw.githubusercontent.com/santarosalia/hopedope/master/public/img/thumb.jpeg"}});';
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <div className="App">
-      
-      
       <header className="header">
         <Link to={"/"}>Home</Link>
         <Link to={"/about"}>소개</Link>
@@ -37,15 +43,6 @@ const App = () => {
       </footer>
       <Helmet>
         <script src="//developers.kakao.com/sdk/js/kakao.min.js"></script>
-        <script type="text/javascript">Kakao.init("7d350e6dfaeae1fb62dd5827979d4a88");
-      Kakao.Link.createScrapButton({
-        container: "#kakao-link-btn",
-        requestUrl: "https://santarosalia.github.io/hopedope/",
-        templateId: 75771,
-        templateArgs: {
-          THU: "https://raw.githubusercontent.com/santarosalia/hopedope/master/public/img/thumb.jpeg"
-        }
-      });</script>
       </Helmet>
     </div>
   );
