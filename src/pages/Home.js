4cc173ded6353beb995c9d1081f31175c62b7380
dope <mdc@kakao.com>
2022-05-02 13:02:28 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,117 +17,130 @@ const Home = () => {
       pw: "",
     },
   ]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  allMsg().then((results) => {
-    for (let i = 0; i < results.length - 1; i++) {
-      const dic = {
-        name: results[i].properties.name.title[0].text.content,
-        msg: results[i].properties.comment.rich_text[0].text.content,
-        pw: results[i].properties.pw.rich_text[0].text.content,
-      };
-      if (results[i] != "undefined" && results[i] != null) {
+  const ax = () => {
+    allMsg().then((results) => {
+      setLoading(true);
+      setError(null);
+      setResult([
+        {
+          name: "",
+          msg: "",
+          pw: "",
+        },
+      ]);
+      for (let i = 0; i < results.length - 1; i++) {
+        const dic = {
+          name: results[i].properties.name.title[0].text.content,
+          msg: results[i].properties.comment.rich_text[0].text.content,
+          pw: results[i].properties.pw.rich_text[0].text.content,
+        };
+
         setResult([...result, dic]);
       }
-      result.map((val) => {
-        console.log(val.name);
-        console.log(val.msg);
-      });
-    }
-  });
+      setLoading(true);
+    });
+  };
+  useEffect(() => {
+    ax();
+  }, []);
 
   // console.log(result[0].properties.name.title[0].text.content);
   // console.log(result[0].properties.comment.rich_text[0].text.content);
   // console.log(result[0].properties.pw.rich_text[0].text.content);
-
-  return (
-    <div className="home">
-      <div className="body">
-        <div className="box">안녕하세용</div>
-        <div id="react-naver-map" className="naverMap">
-          <RenderAfterNavermapsLoaded ncpClientId={"chnhlyto0x"}>
-            <NaverMap
-              id="react-naver-map"
-              style={{
-                width: "100%",
-                height: "250px",
-              }}
-              defaultCenter={{ lat: 37.521602, lng: 126.890874 }}
-              defaultZoom={15}
-            >
-              <Marker
-                position={{ lat: 37.521602, lng: 126.890874 }}
-                onClick={() => {
-                  alert("here");
+  if (loading) return <div>로딩중</div>;
+  if (!loading)
+    return (
+      <div className="home">
+        <div className="body">
+          <div className="box">안녕하세용</div>
+          <div id="react-naver-map" className="naverMap">
+            <RenderAfterNavermapsLoaded ncpClientId={"chnhlyto0x"}>
+              <NaverMap
+                id="react-naver-map"
+                style={{
+                  width: "100%",
+                  height: "250px",
                 }}
-                title={"우리집"}
-              />
-            </NaverMap>
-          </RenderAfterNavermapsLoaded>
-        </div>
-        <div>
-          <>
-            <Swiper
-              effect={"coverflow"}
-              grabCursor={true}
-              centeredSlides={true}
-              slidesPerView={"auto"}
-              coverflowEffect={{
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows: false,
-              }}
-              pagination={false}
-              modules={[EffectCoverflow, Pagination]}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <h5></h5>
-                <p></p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h5>123</h5>
-                <p>456</p>
-              </SwiperSlide>
-            </Swiper>
-          </>
+                defaultCenter={{ lat: 37.521602, lng: 126.890874 }}
+                defaultZoom={15}
+              >
+                <Marker
+                  position={{ lat: 37.521602, lng: 126.890874 }}
+                  onClick={() => {
+                    alert("here");
+                  }}
+                  title={"우리집"}
+                />
+              </NaverMap>
+            </RenderAfterNavermapsLoaded>
+          </div>
+          <div>
+            <>
+              <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: false,
+                }}
+                pagination={false}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <h5></h5>
+                  <p></p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <h5>123</h5>
+                  <p>456</p>
+                </SwiperSlide>
+              </Swiper>
+            </>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
