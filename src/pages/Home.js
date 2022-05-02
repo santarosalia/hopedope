622ac018e176486c8d6f9ffa176972888d0d8333
dope <mdc@kakao.com>
2022-05-02 13:55:35 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
       pw: "",
     },
   ]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const ax = () => {
     allMsg().then((results) => {
