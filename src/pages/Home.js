37e0fd609745579fbcba3a995e53f87d4cce693a
dope <mdc@kakao.com>
2022-05-02 21:59:36 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,11 +34,11 @@ const Home = () => {
     console.log(results);
     setResult(results);
 
-    console.log("result?" + result);
+    console.log("result?string in" + JSON.stringify(result));
   };
   useEffect(() => {
     ax();
-    console.log("리절트?" + result);
+    console.log("result?string out" + JSON.stringify(result));
   }, []);
 
   // console.log(result[0].properties.name.title[0].text.content);
