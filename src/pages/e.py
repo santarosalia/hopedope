3157021ee7ac4b7c6015cf79614c62b3eb3ce4ac
dope <mdc@kakao.com>
2022-05-03 18:40:27 +0900

    a

---
+++ src/pages/e.py
@@ -0,0 +1,20 @@
+from pywinauto.application import Application
+import pywinauto
+
+# app = Application(backend="uia").start("C:\Program Files\Microsoft Office\\root\Office16\EXCEL.EXE")
+
+# dlg = app['Excel']
+
+# a = dlg.child_window(title="새 통합 문서", control_type="ListItem")
+# a.click_input()
+
+# #dlg.print_control_identifiers()
+
+
+# dlg['"A" 2'].select()
+# d= dlg['"A" 2']
+# d.type_keys("123456")
+
+# d.draw_outline('red')
+
+
