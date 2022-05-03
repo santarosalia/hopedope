be77120a21f0e0522b364997eeb4ba322f29a61c
dope <mdc@kakao.com>
2022-05-03 21:47:15 +0900

    a

--- src/pages/Msg.js
+++ src/pages/Msg.js
@@ -25,33 +25,41 @@ const Msg = () => {
     <div className="msg">
       <h3>메시지 써주세요</h3>
       <div className="commentBox">
-        <input placeholder="이름" className="name" onChange={onChange}></input>
-        <input
-          placeholder="비밀번호"
-          className="pw"
-          onChange={onChange}
-          type="password"
-          maxLength={4}
-        ></input>
-        <br></br>
-        <textarea
-          placeholder="글"
-          className="comment"
-          onChange={onChange}
-          rows="2"
-          cols="33"
-        ></textarea>
-        <br></br>
-
-        <button
-          onClick={() =>
-            addComment(name, comment, pw).then(() => {
-              window.location.reload();
-            })
-          }
-        >
-          등록
-        </button>
+        <form>
+          <input
+            required
+            placeholder="이름"
+            className="name"
+            onChange={onChange}
+          ></input>
+          <input
+            required
+            placeholder="비밀번호"
+            className="pw"
+            onChange={onChange}
+            type="password"
+            maxLength={4}
+          ></input>
+          <br></br>
+          <textarea
+            placeholder="글"
+            className="comment"
+            onChange={onChange}
+            rows="2"
+            cols="33"
+          ></textarea>
+          <br></br>
+          <button
+            type="submit"
+            onClick={() =>
+              addComment(name, comment, pw).then(() => {
+                window.location.reload();
+              })
+            }
+          >
+            등록
+          </button>
+        </form>
       </div>
     </div>
   );
