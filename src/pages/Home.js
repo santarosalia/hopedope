9e82ec1d99384e8c7f45873c47c7c7b3cf72b7b2
dope <mdc@kakao.com>
2022-05-04 17:12:23 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,7 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useSpring, animated } from "react-spring";
 
 const Fade = React.forwardRef(function Fade(props, ref) {
   const { in: open, children, onEnter, onExited, ...other } = props;
