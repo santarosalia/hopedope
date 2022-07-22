225e1003fca3a58149b88c2066e965fcec9bf247
dope <mdc@kakao.com>
2022-07-22 14:15:26 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,9 +43,9 @@ const Home = () => {
   });
 
   const Myswiper = () => {
+    console.log("slider");
+    if (result == null) return;
     const MySlider = () => {
-      console.log("slider");
-      if (result == null) return;
       const resultList = result.map((item) => {
         <SwiperSlide>
           <h4>{item.name}</h4>
