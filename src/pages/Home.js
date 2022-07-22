46ab8c29e910f96dc8d6923ed762f86511544412
dope <mdc@kakao.com>
2022-07-22 13:05:11 +0900

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,6 +24,7 @@ const style = {
   p: 4,
 };
 const { allMsg } = require("../axios");
+
 const Home = () => {
   const [result, setResult] = useState(null);
   const [open1, setOpen1] = React.useState(false);
@@ -41,6 +42,115 @@ const Home = () => {
       setResult(results);
     });
   });
+
+  const swiper = () => {
+    return (
+      <>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={false}
+          modules={[EffectCoverflow, Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          {/* style={{ backgroundColor: "white" }} */}
+
+          <SwiperSlide>
+            <div>
+              <h4>{result[0].name}</h4>
+              <p>{result[0].msg}</p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[1].name}</h4>
+            <p>{result[1].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[2].name}</h4>
+            <p>{result[2].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[3].name}</h4>
+            <p>{result[3].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[4].name}</h4>
+            <p>{result[4].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[5].name}</h4>
+            <p>{result[5].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[6].name}</h4>
+            <p>{result[6].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[7].name}</h4>
+            <p>{result[7].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[8].name}</h4>
+            <p>{result[8].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[9].name}</h4>
+            <p>{result[9].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[10].name}</h4>
+            <p>{result[10].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[11].name}</h4>
+            <p>{result[11].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[12].name}</h4>
+            <p>{result[12].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[13].name}</h4>
+            <p>{result[13].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[14].name}</h4>
+            <p>{result[14].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[15].name}</h4>
+            <p>{result[15].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[16].name}</h4>
+            <p>{result[16].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[17].name}</h4>
+            <p>{result[17].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[18].name}</h4>
+            <p>{result[18].msg}</p>
+          </SwiperSlide>
+          <SwiperSlide>
+            <h4>{result[19].name}</h4>
+            <p>{result[19].msg}</p>
+          </SwiperSlide>
+        </Swiper>
+      </>
+    );
+  };
 
   if (result == null)
     return (
@@ -123,111 +233,8 @@ const Home = () => {
           </div>
         </div>
 
-        <div>
-          <>
-            <Swiper
-              effect={"coverflow"}
-              grabCursor={true}
-              centeredSlides={true}
-              slidesPerView={"auto"}
-              autoplay={{ delay: 2500, disableOnInteraction: false }}
-              coverflowEffect={{
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows: false,
-              }}
-              pagination={false}
-              modules={[EffectCoverflow, Pagination, Autoplay]}
-              className="mySwiper"
-            >
-              {/* style={{ backgroundColor: "white" }} */}
-
-              <SwiperSlide>
-                <div>
-                  <h4>{result[0].name}</h4>
-                  <p>{result[0].msg}</p>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[1].name}</h4>
-                <p>{result[1].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[2].name}</h4>
-                <p>{result[2].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[3].name}</h4>
-                <p>{result[3].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[4].name}</h4>
-                <p>{result[4].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[5].name}</h4>
-                <p>{result[5].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[6].name}</h4>
-                <p>{result[6].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[7].name}</h4>
-                <p>{result[7].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[8].name}</h4>
-                <p>{result[8].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[9].name}</h4>
-                <p>{result[9].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[10].name}</h4>
-                <p>{result[10].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[11].name}</h4>
-                <p>{result[11].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[12].name}</h4>
-                <p>{result[12].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[13].name}</h4>
-                <p>{result[13].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[14].name}</h4>
-                <p>{result[14].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[15].name}</h4>
-                <p>{result[15].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[16].name}</h4>
-                <p>{result[16].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[17].name}</h4>
-                <p>{result[17].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[18].name}</h4>
-                <p>{result[18].msg}</p>
-              </SwiperSlide>
-              <SwiperSlide>
-                <h4>{result[19].name}</h4>
-                <p>{result[19].msg}</p>
-              </SwiperSlide>
-            </Swiper>
-          </>
+        <div className="swiper">
+          <swiper />
         </div>
       </div>
     </div>
