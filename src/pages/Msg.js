8f80d9574f1eb12ffafdc4e08d58b23bf67be993
dope <mdc@kakao.com>
2022-07-26 17:55:35 +0900

    a

--- src/pages/Msg.js
+++ src/pages/Msg.js
@@ -3,7 +3,7 @@ import TextField from "@mui/material/TextField";
 import "./msg.css";
 
 const { addComment } = require("../axios");
-import { makeStyles } from "@material-ui/core";
+import { makeStyles } from "@mui/material-ui/core";
 import { height } from "@mui/system";
 import { FormGroup } from "@mui/material";
 
